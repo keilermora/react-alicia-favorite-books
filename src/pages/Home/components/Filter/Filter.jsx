aac9141fa76d1988d3d4cb34608f659e49e50799
Keiler Mora <keilermora@gmail.com>
2017-09-18 14:36:26 -0300

    Responsividad del filtro de búsqueda
    Se mejoró la responsividad del filtro de búsqueda y del contenedor de los resultados de la búsqueda.

--- src/pages/Home/components/Filter/Filter.jsx
+++ src/pages/Home/components/Filter/Filter.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, Form, FormControl, FormGroup, Glyphicon, InputGroup } from 'react-bootstrap';
+import { Button, Col, Form, FormControl, FormGroup, Glyphicon, Grid, InputGroup, Row } from 'react-bootstrap';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
@@ -21,21 +21,25 @@ class Filter extends Component {
     this.updateFilterType = this.updateFilterType.bind(this);
   }
 
+  // Cambiar a la vista tipo aplicación
   toggleAppView(e) {
     e.preventDefault();
     return this.props.actions.setView('app-view');
   }
 
+  // Cambiar a la vista tipo lista
   toggleListView(e) {
     e.preventDefault();
     return this.props.actions.setView('list-view');
   }
 
+  // Actualizar el texto del filtro
   updateFilterText(e) {
     e.preventDefault();
     return this.props.actions.setFilterText(e.target.value);
   }
 
+  // Actualizar el tipo de filtro
   updateFilterType(e) {
     return this.props.actions.setFilterType(e.target.value);
   }
@@ -43,39 +47,51 @@ class Filter extends Component {
   render() {
     return (
       <Form id="filter-container" inline>
-        <FormGroup>
-          <Button onClick={this.toggleAppView} active={this.props.viewMode === 'app-view'}>
-            <Glyphicon glyph="th" />
-          </Button>
-        </FormGroup>
-        <FormGroup>
-          <Button onClick={this.toggleListView} active={this.props.viewMode === 'list-view'}>
-            <Glyphicon glyph="th-list" />
-          </Button>
-        </FormGroup>
-        <FormGroup>
-          <FormControl
-            componentClass="select"
-            defaultValue={this.props.filterType}
-            onChange={this.updateFilterType}
-          >
-            <option value="title">Título </option>
-            <option value="author">Autor </option>
-            <option value="gender">Género </option>
-          </FormControl>
-        </FormGroup>
-        <FormGroup>
-          <InputGroup>
-            <InputGroup.Addon>
-              <Glyphicon glyph="search" />
-            </InputGroup.Addon>
-            <FormControl
-              onChange={this.updateFilterText}
-              value={this.props.filterText}
-              type="text"
-            />
-          </InputGroup>
-        </FormGroup>
+        <Grid>
+          <Row>
+            <Col xs={6} sm={2} smOffset={2} md={1} mdOffset={2}>
+              <FormGroup>
+                <Button onClick={this.toggleAppView} active={this.props.viewMode === 'app-view'}>
+                  <Glyphicon glyph="th" />
+                </Button>
+              </FormGroup>
+            </Col>
+            <Col xs={6} sm={2} md={1}>
+              <FormGroup>
+                <Button onClick={this.toggleListView} active={this.props.viewMode === 'list-view'}>
+                  <Glyphicon glyph="th-list" />
+                </Button>
+              </FormGroup>
+            </Col>
+            <Col xs={12} sm={4} md={2}>
+              <FormGroup>
+                <FormControl
+                  componentClass="select"
+                  defaultValue={this.props.filterType}
+                  onChange={this.updateFilterType}
+                >
+                  <option value="title">Título </option>
+                  <option value="author">Autor </option>
+                  <option value="gender">Género </option>
+                </FormControl>
+              </FormGroup>
+            </Col>
+            <Col xs={12} sm={8} smOffset={2} md={4} mdOffset={0}>
+              <FormGroup>
+                <InputGroup>
+                  <InputGroup.Addon>
+                    <Glyphicon glyph="search" />
+                  </InputGroup.Addon>
+                  <FormControl
+                    onChange={this.updateFilterText}
+                    value={this.props.filterText}
+                    type="text"
+                  />
+                </InputGroup>
+              </FormGroup>
+            </Col>
+          </Row>
+        </Grid>
       </Form>
     );
   }
