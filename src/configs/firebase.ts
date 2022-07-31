abfbfb49d6ccc30ad7bda5efae16b04fb8e8b1da
Keiler Mora <keilermora@gmail.com>
2022-07-31 18:27:14 -0400

    feat: add an .env.example file and maintain .env file for now

---
+++ src/configs/firebase.ts
@@ -0,0 +1,24 @@
+import { FirebaseOptions, initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const {
+  REACT_APP_FIREBASE_API_KEY,
+  REACT_APP_FIREBASE_AUTH_DOMAIN,
+  REACT_APP_FIREBASE_PROJECT_ID,
+  REACT_APP_FIREBASE_STORAGE_BUCKET,
+  REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  REACT_APP_FIREBASE_APP_ID,
+} = process.env;
+
+const firebaseConfig: FirebaseOptions = {
+  apiKey: REACT_APP_FIREBASE_API_KEY,
+  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: REACT_APP_FIREBASE_APP_ID,
+};
+
+initializeApp(firebaseConfig);
+
+export const db = getFirestore();
