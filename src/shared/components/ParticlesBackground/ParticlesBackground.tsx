48d9084741a0e11bee9e14d3bb84062184d84ae2
Keiler Mora <keilermora@gmail.com>
2022-07-31 19:34:00 -0400

    fix(particles-background): update the way that particles should be renderer on newest tsparticles -v

--- src/shared/components/ParticlesBackground/ParticlesBackground.tsx
+++ src/shared/components/ParticlesBackground/ParticlesBackground.tsx
@@ -1,19 +1,25 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import Particles from 'react-tsparticles';
 
 import particles from 'assets/particles.json';
 
 import styles from './ParticlesBackground.module.css';
+import { Engine, ISourceOptions } from 'tsparticles-engine';
+import { loadFull } from 'tsparticles';
 
 interface ParticlesBackgroundProps {
   children: React.ReactNode;
 }
 
 const ParticlesBackground = ({ children }: ParticlesBackgroundProps): JSX.Element => {
-  const particlesOptions = particles;
+  const particlesInit = useCallback(async (engine: Engine) => {
+    await loadFull(engine);
+  }, []);
+
+  const particlesOptions: ISourceOptions = particles;
   return (
     <>
-      <Particles className={styles.particles} options={particlesOptions} />
+      <Particles className={styles.particles} options={particlesOptions} init={particlesInit} />
       {children}
     </>
   );
